6b0e8ed8581ede55c69b8f3bf74c03b6debb798c
Shazam <40243053+schazadm@users.noreply.github.com>
2023-10-10 22:30:32 +0200

    continue basic layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,25 @@
 import { javascript } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
 import CodeMirror from "@uiw/react-codemirror";
 import React, { useState } from "react";
+import { Responsive, WidthProvider } from "react-grid-layout";
 import Output from "./Output";
 import Prompt from "./Prompt";
-import ResizableTile from "./ResizeableTile";
 import Tests from "./Tests";
 
+import "react-grid-layout/css/styles.css";
 import "./App.css";
-import "/node_modules/react-grid-layout/css/styles.css";
-import "/node_modules/react-resizable/css/styles.css";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 function App() {
+  const layout = [
+    { i: "1", x: 0, y: 0, w: 4, h: 4, minW: 2 },
+    { i: "2", x: 6, y: 0, w: 8, h: 4, minW: 2 },
+    { i: "3", x: 0, y: 6, w: 6, h: 2, minW: 2 },
+    { i: "4", x: 6, y: 6, w: 6, h: 2, minW: 2 },
+  ];
+
   const [code, setCode] = useState("");
   const [output, setOutput] = useState("");
   const tests = [
@@ -20,7 +29,7 @@ function App() {
 
   const onChange = React.useCallback((value, viewUpdate) => {
     // TODO: Track every change
-    // console.log("value:", value);
+    console.log("value:", value);
   }, []);
 
   const runCode = () => {
@@ -31,26 +40,39 @@ function App() {
 
   return (
     <div className="app">
-      <ResizableTile>
-        <Prompt promptText="Write a function that adds two numbers." />
-      </ResizableTile>
-
-      <ResizableTile width={400} height={300}>
-        {/* <CodeEditor code={code} onChange={handleCodeChange} /> */}
-        <CodeMirror
-          extensions={[javascript({ jsx: true })]}
-          onChange={onChange}
-        />
-        <button onClick={runCode}>Run Code</button>
-      </ResizableTile>
-
-      <ResizableTile>
-        <Output output={output} />
-      </ResizableTile>
-
-      <ResizableTile>
-        <Tests tests={tests} />
-      </ResizableTile>
+      <ResponsiveGridLayout
+        className="layout"
+        layouts={{ lg: layout }}
+        // breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
+        cols={{ lg: 12, md: 12, sm: 6, xs: 2, xxs: 1 }}
+        // rowHeight={100}
+        // width={1200}
+        draggableHandle=".react-grid-dragHandle"
+      >
+        <div key="1" className="resizable-tile">
+          <Prompt promptText="Write a function that adds two numbers." />
+          <span className="react-grid-dragHandle">[DRAG]</span>
+        </div>
+        <div key="2" className="resizable-tile">
+          <CodeMirror
+            value=""
+            extensions={python()}
+            onChange={onChange}
+            height="500px"
+          />
+          <button onClick={runCode}>Run Code</button>
+          <span className="react-grid-dragHandle">[DRAG]</span>
+        </div>
+        <div key="3" className="resizable-tile">
+          <Tests tests={tests} />
+          <span className="react-grid-dragHandle">[DRAG]</span>
+        </div>
+        <div key="4" className="resizable-tile">
+          <Output output={output} />
+          <button onClick={runCode}>Submit Code</button>
+          <span className="react-grid-dragHandle">[DRAG]</span>
+        </div>
+      </ResponsiveGridLayout>
     </div>
   );
 }
